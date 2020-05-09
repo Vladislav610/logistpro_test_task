d3bc472d3b3eaee21002d24016ff422f6cc81274
Vladislav610 <vladislaveremeev610@yandex.ru>
2020-05-09 14:19:47 +0400

    testcase3 - fixed

--- Pages/product_page.py
+++ Pages/product_page.py
@@ -6,14 +6,17 @@ from selenium.common.exceptions import NoSuchElementException
 
 class ProductPage(BasePage):
     def selected_offer_should_be_added_to_cart(self):
+        offer_name_in_page = self.browser.find_element (By.CSS_SELECTOR, 'div.col>h1')
+        offer_name_in_page = offer_name_in_page.text
         add_to_cart_button = self.browser.find_element(By.CLASS_NAME, "btn-outline-success")
         add_to_cart_button.click()
-        self.browser.get('https://bamboo.dev.sozvezdie-tour.ru/cart/')
-        offer_name = self.browser.find_element (By.XPATH, '//*/h1')
-        offer_name = offer_name.get_attribute('text')
-        link = self.browser.find_element(By.XPATH, '//table//a')
-        link = link.get_attribute('href')
-        self.browser.get(link)
-        ok = self.browser.find_element(By.XPATH, '//table//a')
-        ok = ok.get_attribute('text')
-        assert ok, 'Выбранное предложение не добавилось в корзину'
+        go_to_cart_link = self.browser.find_element(By.XPATH, '//tbody/tr[1]/td[4]/a')
+        go_to_cart_link = go_to_cart_link.get_attribute('href')
+        self.browser.get(go_to_cart_link)
+        offer_name_in_cart = self.browser.find_element(By.CSS_SELECTOR, 'td:nth-child(1)>a')
+        offer_name_in_cart = offer_name_in_cart.text
+        assert offer_name_in_page == offer_name_in_cart,\
+            'Выбранное предложение не добавилось в корзину'
+
+    def can_buy_offer_added_to_cart(self):
+        pass
--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,7 @@
 from .Pages.product_page import ProductPage
 from pytest import mark
 
+
 @mark.smoke
 @mark.testcase3
 def test_can_add_to_cart(browser):
@@ -12,3 +13,15 @@ def test_can_add_to_cart(browser):
     page = ProductPage(browser, link)
     page.open()
     page.selected_offer_should_be_added_to_cart()
+
+@mark.smoke
+@mark.testcase4
+def can_buy_offer_added_to_cart(browser):
+    """
+    Проверка возможности купить
+    предложение, добавленное в корзину
+    """
+    link = "https://bamboo.dev.sozvezdie-tour.ru/cart/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.can_buy_offer_added_to_cart()
